76d0e63b6de034f48eb87c693c43899f319ac5c5
Scott Rabin <scottrabin@gmail.com>
2012-01-07 14:26:16 -0500

    Added new demonstrations for additional functionality

--- demo/demo.js
+++ demo/demo.js
@@ -3,4 +3,14 @@ $(function(){
         .on('click', '.clickquick', function(){
             $(this).quickConfirm();
         });
+
+    $('[rel]').each(function(){
+        var code = $( $(this).attr('rel') );
+        if( code.length > 0 ){
+            code = code.text();
+            $(this).bind( 'click', function() {
+                eval( code );
+            });
+        }
+    });
 });
